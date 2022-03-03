045fccca1ced6605a3047c429d64bece2115d8e3
Riley Johnson <riley.scott.johnson@gmail.com>
2022-03-02 20:28:14 -0500

    print_menu completed

--- assignment_2/acc.c
+++ assignment_2/acc.c
@@ -31,9 +31,32 @@ void print_acc(short acc, char mode) {
 }
 
 char print_menu(void) {
+    short need_input = 1;
+    char choice;
+    while (need_input){
+        char valid_input [6] = "OHCSQ";
+        printf("Please select one of the following options:\n\n");
+        printf("O  Octal Mode\n");
+        printf("H  Hexidecimal Mode\n\n");
+        printf("C  Clear Accumulator\n");
+        printf("S  Set Accumulator\n\n");
+        printf("Q  Quit\n\n");
+        printf("Option: ");
 
+        char input [10];
+        scanf("%s", &input);
+        printf("%s\n", input);
+        choice = toupper(input[0]);
+
+        if (strlen(input) > 1 || !strchr(valid_input, choice)) {
+            printf("Invalid option: %s\n\n", input);
+        } else {
+            need_input = 0;
+        }
+    }
+    return choice;
 }
 
 int main(void) {
-    
+
 }
