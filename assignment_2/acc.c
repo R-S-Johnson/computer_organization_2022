3fb3ae45d5af9021b17eb21f57e6e3a1ce77d1f5
Riley Johnson <riley.scott.johnson@gmail.com>
2022-03-02 19:25:46 -0500

    get_operand funciton complete

--- assignment_2/acc.c
+++ assignment_2/acc.c
@@ -3,11 +3,27 @@
 
 short get_operand(char mode) {
     short input;
-    if (mode == 'D') {
-        printf("Enter decimal value: ");
-        scanf("%d", &input);
-        printf("%d\n", input);
+    switch (mode) {
+        case 'D': {
+            printf("Enter decimal value: ");
+            scanf("%d", &input);
+            printf("%d\n", input);
+            break;
+        }
+        case 'H': {
+            printf("Enter hex value: ");
+            scanf("%X", &input);
+            printf("%X\n", input);
+            break;
+        }
+        case 'O': {
+            printf("Enter octal value: ");
+            scanf("%o", &input);
+            printf("%o\n", input);
+            break;
+        }
     }
+    return input;
 }
 
 void print_acc(short acc, char mode) {
@@ -19,6 +35,5 @@ char print_menu(void) {
 }
 
 int main(void) {
-    char input = 'D';
-    get_operand(input);
+    
 }
