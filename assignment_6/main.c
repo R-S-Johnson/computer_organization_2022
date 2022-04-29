d40ac4df023c3285f3ebf5e764763136bf9f4af1
Riley Johnson <riley.scott.johnson@gmail.com>
2022-04-29 18:05:43 -0400

    main done

--- assignment_6/main.c
+++ assignment_6/main.c
@@ -6,7 +6,18 @@
 #include "contact_list.h"
 
 void process_file(char * filename) {
+    FILE *fp;
+    fp = fopen(filename, 'r');
 
+    fscanf(fp,"%*s\n");
+
+    CONTACT_T * input;
+
+    while (fscanf(fp, "[^;];[^;];[^;];[^;];[^;];[^;];[^;];[^;];[^;];[^;];[^;];%s ", input->first_name, input->last_name, input->co_name,
+    input->addr, input->city, input->county, input->state, input->zip, input->phone1, input->phone2, input->email, input->web) == 12) {
+        CONTACT_T * newContact = create_contact(input);
+        insert_contact(newContact);
+    }
 }
 
 char print_menu(void) {
@@ -52,15 +63,46 @@ char print_format_menu(void){
 }
 
 void find_individual(char mode) {
+    NAME_T * firstName;
+    NAME_T * lastName;
+    printf("Please enter last name: ");
+    scanf("%s", &lastName);
+    printf("%s\n", lastName);
+    
+    printf("Please enter first name: ");
+    scanf("%s", &firstName);
+    printf("%s\n", firstName);
 
+    CONTACT_T * foundContact = find_contact(lastName, firstName);
+    print_contact(foundContact, mode);
 }
 
 void print_tab(char mode) {
+    char * tabLetter;
+    printf("Please enter last name tab letter: ");
+    scanf("%s", &tabLetter);
+    printf("%s\n", tabLetter);
+    upper(tabLetter);
 
+    CONTACT_T * returnedContact = get_next_contact(tabLetter[0]);
+    if (returnedContact == NULL) {
+        printf("Contact was not found!");
+    }
+    else {
+        while (returnedContact != NULL) {
+            print_contact(returnedContact, mode);
+            returnedContact = get_next_contact(tabLetter[0]);
+        }
+    }
 }
 
 void process_state_contacts(char mode) {
-
+    char * stateChoice;
+    printf("Please enter two-letter state abbreviation: ");
+    scanf("%s", &stateChoice);
+    printf("%s\n", stateChoice);
+    upper(stateChoice);
+    print_state_contacts(stateChoice, mode);
 }
 
 int main(int argc, char const *argv[])
